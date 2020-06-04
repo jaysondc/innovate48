73cee582ce6b7ef98c8f1a9dd5f40be62090dbc7
tyler.hu <Tyler.Hu@fisglobal.com>
2020-06-03 22:02:50 -0700

    "added jayson 80% of jaysons images"

--- SnapLens/Public/onCardTap.js
+++ SnapLens/Public/onCardTap.js
@@ -5,31 +5,32 @@ cat and pics are 2 variables declared below in coments, and
 thus any scene object using this script will obtain
 these variables. However, they must be  
 */
-//@input Component.Image image
-//@input Asset.Texture texture
-//@input Asset.Texture texture2
+//@input SceneObject spendings 
+//@input SceneObject transactions
 // Set the Image component's texture
 
-script.image.mainPass.baseTex = script.texture;
-var count = false 
-
+//script.image.mainPass.baseTex = script.texture;
+var count = true 
+script.spendings.enabled = true;
+script.transactions.enabled = false; 
 // Set the Image's pivot point
 //script.image.pivot = new vec2(1, 1);
 
 //var count = 0 
 //script.cat.mainPass.baseTex = script.pics[count] //mainPass allows us to reference to cat and base Text references to the base texture of that which is a picture of my cat
-print("LAST ONE")
+print("Tap Initialize")
 function onTapped(eventData)
 {
-    count = !count;
+    
     if (count){
-    script.image.mainPass.baseTex = script.texture;    
+        script.spendings.enabled = false;
+        script.transactions.enabled = true;     
     }    
     else{
-    script.image.mainPass.baseTex = script.texture2;      
+        script.spendings.enabled = true;
+        script.transactions.enabled = false;  
     }
-    
- 
+   count = !count;
     print("Tap Position: (" + eventData.getTapPosition().x + ", " + eventData.getTapPosition().y + ")");
 }
 
